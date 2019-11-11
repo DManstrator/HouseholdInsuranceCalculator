daa36d31e088edcd9e0eb161dace5e88bc224d41
DManstrator <DManstrator@gmail.com>
2019-11-11 01:45:12 +0100

    Forgot to update the Main Docs somehow

--- src/test/java/householdinsurancecalculator/Tests.java
+++ src/test/java/householdinsurancecalculator/Tests.java
@@ -11,28 +11,24 @@ public abstract class Tests {
     
     /**
      * Tests a compact insurance product with 20 square meters.
-     * Result should be 650 Euros * 20 square meters = 13,000 euros.
      */
     @Test
     abstract public void testCompactWithTwentySquareMeter();
     
     /**
      * Tests an ideal insurance product with 15 square meters.
-     * Result should be 700 euros * 15 square meters = 10,500 euros.
      */
     @Test
     abstract public void testIdealWithFifteenSquareMeter();
     
     /**
      * Tests an ideal insurance product name with 10 square meters by its name.
-     * Result should be 700 Euros * 10 square meters = 7,000 euros.
      */
     @Test
     abstract public void testValidInsuranceProductNameWithTenSquareMeter();
     
     /**
      * Tests a compact insurance product name in lowercase with 30 square meters by its name.
-     * Result should be 650 Euros * 30 square meters = 19,500 euros.
      */
     @Test
     abstract public void testValidLowercaseInsuranceProductNameWithThirtySquareMeter();
